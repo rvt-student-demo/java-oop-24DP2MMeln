496f13b36fa14ce875ecb9eda43eff2b234748f9
24DP2MMeln <A240532mm@rvt.lv>
2026-03-24 09:10:59 +0200

    Initial commit

---
+++ src/main/java/rvt/shop/Item.java
@@ -0,0 +1,28 @@
+package main.java.rvt.shop;
+import java.util.*;
+
+public class Item {
+    public static void main(String[] args) {
+        Item item = new Item("milk", 4, 2);
+        System.out.println("an item that contains 4 milks has the total price of " + item.price());
+        System.out.println(item);
+        item.increaseQuantity();
+        System.out.println(item);
+    }
+
+    public Item(String product, int qty, int unitPrice) {
+
+    }
+
+    public int price() {
+
+    }
+
+    public void increaseQuality() {
+
+    }
+
+    public String toString {
+
+    }
+}
---
+++ src/main/java/rvt/shop/Warehouse.java
@@ -0,0 +1,61 @@
+package main.java.rvt.shop;
+import java.util.*;
+
+public class Warehouse {
+
+    private Map<String, Integer> prices;
+    private Map<String, Integer> stocks;
+
+    public Warehouse() {
+        this.prices = new HashMap<>();
+        this.stocks = new HashMap<>();
+    }
+
+    public void addProduct(String product, int price, int stock) {
+        this.prices.put(product, price);
+        this.stocks.put(product, stock);
+    }
+
+
+    public int price(String product) {
+        if (this.prices.containsKey(product)) {
+            return this.prices.get(product);
+        }
+        return - 99;
+    }
+
+    public boolean take (String product) {
+        return false; //placeholder
+
+    }
+
+    public int stock (String product) {
+        return 0; //placeholder
+
+    }
+
+    public set<String> products() {
+
+    }
+
+
+
+public static void main(String[] args) {
+
+    Warehouse warehouse = new Warehouse();
+    warehouse.addProduct("coffee", 5, 1);
+
+    System.out.println("stock:");
+    System.out.println("coffee:  " + warehouse.stock("coffee"));
+    System.out.println("sugar: " + warehouse.stock("sugar"));
+
+    System.out.println("taking coffee " + warehouse.take("coffee"));
+    System.out.println("taking coffee " + warehouse.take("coffee"));
+    System.out.println("taking sugar " + warehouse.take("sugar"));
+
+    System.out.println("stock:");
+    System.out.println("coffee:  " + warehouse.stock("coffee"));
+    System.out.println("sugar: " + warehouse.stock("sugar"));
+    
+}
+}
