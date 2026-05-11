690a0ed3de97baa237ea12e2e05c9225cb5abcbf
24DP2MMeln <A240532mm@rvt.lv>
2026-05-11 15:00:34 +0300

    Interface in a box

---
+++ src/main/java/rvt/InterFaceInABox.java
@@ -0,0 +1,116 @@
+package main.java.rvt;
+
+import java.util.ArrayList;
+
+public class InterFaceInABox {
+    public static void main(String[] args) {
+
+        Book book1 = new Book("Fyodor Dostoevsky", "Crime and Punishment", 2);
+        Book book2 = new Book("Robert Martin", "Clean Code", 1);
+        Book book3 = new Book("Kent Beck", "Test Driven Development", 0.5);
+
+        CD cd1 = new CD("Pink Floyd", "Dark Side of the Moon", 1973);
+        CD cd2 = new CD("Wigwam", "Nuclear Nightclub", 1975);
+        CD cd3 = new CD("Rendezvous Park", "Closer to Being Here", 2012);
+
+        System.out.println(book1);
+        System.out.println(book2);
+        System.out.println(book3);
+        System.out.println(cd1);
+        System.out.println(cd2);
+        System.out.println(cd3);
+
+        Box box = new Box(10);
+
+        box.add(book1);
+        box.add(book2);
+        box.add(book3);
+
+        box.add(cd1);
+        box.add(cd2);
+        box.add(cd3);
+
+        System.out.println(box);
+    }
+}
+
+interface Packable {
+    double weight();
+}
+
+class Book implements Packable {
+
+    private String author;
+    private String name;
+    private double weight;
+
+    public Book(String author, String name, double weight) {
+        this.author = author;
+        this.name = name;
+        this.weight = weight;
+    }
+
+    @Override
+    public double weight() {
+        return weight;
+    }
+
+    @Override
+    public String toString() {
+        return author + ": " + name + " (" + weight + " kg)";
+    }
+}
+
+class CD implements Packable {
+
+    private String artist;
+    private String name;
+    private int year;
+
+    public CD(String artist, String name, int year) {
+        this.artist = artist;
+        this.name = name;
+        this.year = year;
+    }
+
+    @Override
+    public double weight() {
+        return 0.1;
+    }
+
+    @Override
+    public String toString() {
+        return artist + ": " + name + " (" + year + ")";
+    }
+}
+
+class Box implements Packable {
+
+    private ArrayList<Packable> items;
+    private double capacity;
+
+    public Box(double capacity) {
+        this.items = new ArrayList<>();
+        this.capacity = capacity;
+    }
+
+    public void add(Packable item) {
+        if (item.weight() + weight() <= capacity) {
+            items.add(item);
+        }
+    }
+
+    @Override
+    public double weight() {
+        double total = 0;
+        for (Packable p : items) {
+            total += p.weight();
+        }
+        return total;
+    }
+
+    @Override
+    public String toString() {
+        return "Box: " + items.size() + " items, total weight " + weight() + " kg";
+    }
+}
