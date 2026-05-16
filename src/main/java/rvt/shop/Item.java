cabc5365fe8facaf456ffa1dc2ceb69f1a2aadf7
24DP2MMeln <A240532mm@rvt.lv>
2026-05-16 18:28:19 +0000

    Item (fixed code)

--- src/main/java/rvt/shop/Item.java
+++ src/main/java/rvt/shop/Item.java
@@ -1,31 +1,35 @@
 package rvt.shop;
-import java.util.*;
-import java.util.HashMap;
 
 public class Item {
+
+    private String product;
+    private int quantity;
+    private int unitPrice;
+
+    public Item(String product, int qty, int unitPrice) {
+        this.product = product;
+        this.quantity = qty;
+        this.unitPrice = unitPrice;
+    }
+
+    public int price() {
+        return this.quantity * this.unitPrice;
+    }
+
+    public void increaseQuantity() {
+        this.quantity += 1;
+    }
+
+    @Override
+    public String toString() {
+        return this.product + ": " + this.quantity;
+    }
+
     public static void main(String[] args) {
         Item item = new Item("milk", 4, 2);
         System.out.println("an item that contains 4 milks has the total price of " + item.price());
         System.out.println(item);
         item.increaseQuantity();
         System.out.println(item);
-    }
-
-    public Item(String product, int qty, int unitPrice) {
-        this.product = product;
-        this.quality = qty;
-        this.unitPrice = price;
-    }
-
-    public int price() {
-        return price;
-    }
-
-    public void increaseQuality() {
-        this.qty += 1;
-    }
-
-    public String toString; {
-        return this.product + ": " + this.qty;
     }
 }
